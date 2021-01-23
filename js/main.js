7adffb535fcdd0b77221e657f73370be5141611f
William Marques <matrix.consultoria@gmail.com>
2021-01-23 13:19:09 -0300

    aula 01 - Introdução ao JavaScript

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var nome = "William Marques";
+var n1 = 5;
+var n2 = 3;
+var frase = "Japão é o melhor time do mundo"
+//alert(nome + " tem "+ idade + " anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toLocaleLowerCase());
+//alert(frase.replace("Japão", "Brasil"));
