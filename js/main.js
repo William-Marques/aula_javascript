8ba1b11bc1edf48398a69e25607b8de10a0412ea
William Marques <matrix.consultoria@gmail.com>
2021-01-23 15:22:20 -0300

    aula 04 - Desenvolva páginas web com JavaScript

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
 
+function soma(n1, n2){
+    return n1 + n2;
+}
 
+var validar=0;
+function validaIdade(idade){
+    if(idade >=18){
+        validar = true;        
+    }else{
+        validar = false;
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade?")
+validaIdade(idade)
+console.log(validar);
+
+//alert(soma(5, 10));
+/*
 var d = new Date();
 alert(d.getDay());
 alert(d.getHours());
 alert(d.getMinutes());
+*/
 
 /*
 var count;
