bc4bc240a89279f487d2a8367b540f8c8ca8a421
William Marques <matrix.consultoria@gmail.com>
2021-01-23 13:53:54 -0300

    aula 02 - Array e Dicionário

--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
-var nome = "William Marques";
-var n1 = 5;
-var n2 = 3;
-var frase = "Japão é o melhor time do mundo"
+
+
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
+console.log(frutas);
+alert(frutas[1].nome);
+
+/*
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+
+//var nome = "William Marques";
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Japão é o melhor time do mundo"
 //alert(nome + " tem "+ idade + " anos");
 //alert(idade + idade2);
-console.log(nome);
-console.log(n1 * n2);
-console.log(frase.toLocaleLowerCase());
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.toLocaleLowerCase());
 //alert(frase.replace("Japão", "Brasil"));
